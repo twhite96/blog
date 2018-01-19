74df9d57fb9545d22abca14e01ed4cf09b0988ab
Tiffany White <tiffanywhitedev412@gmail.com>
2018-01-19 15:02:25 -0500

    Switch to postgres for pushing to Heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,11 @@
 class User < ApplicationRecord
   has_many :posts, dependent: :destroy
 
-  validates :username, presence: true
+  validates :username, presence: true,
+            length: { maximum: 25 }
   validates :email, uniqueness: { allow_blank: true,
             case_sensitive: false },
             length: { maximum: 50 },
             email: true,
-            presence: true, 
-            format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i },
-            allow_blank: true
+            presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180119030933) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
